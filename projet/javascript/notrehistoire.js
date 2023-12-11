b8207179b0035e9c0cd4cde17dff308535fc5ff4
eric4231 <144723670+eric4231@users.noreply.github.com>
2023-12-11 13:05:33 -0500

    Add files via upload

---
+++ projet/javascript/notrehistoire.js
@@ -0,0 +1,6 @@
+const histoire_txt = document.getElementById('histoire_txt')
+const histoire_img = document.getElementById('histoire_img')
+
+histoire_img.addEventListener('mouseover', function() {
+    histoire_txt.style.opacity = '1'
+})
